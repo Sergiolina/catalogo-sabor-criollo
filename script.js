1c51c73f65af0574f2ea963495adca158b61e8cb
Sergiolina <sergiolinaresbarban@gmail.com>
2026-09-09 18:00:35 -0400

    Agregue datos de productos en js

---
+++ script.js
@@ -0,0 +1,32 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Pizza Especial",
+        descripcion: "Pizza con queso, jamón y vegetales.",
+        precio: 350,
+        categoria: "comidas"
+    },
+    {
+        id: 2,
+        nombre: "Hamburguesa Criolla",
+        descripcion: "Carne, queso, vegetales y salsa.",
+        precio: 250,
+        categoria: "comidas"
+    },
+    {
+        id: 3,
+        nombre: "Refresco",
+        descripcion: "Refresco frío.",
+        precio: 100,
+        categoria: "bebidas"
+    },
+    {
+        id: 4,
+        nombre: "Jugo Natural",
+        descripcion: "Jugo preparado al momento.",
+        precio: 150,
+        categoria: "bebidas"
+    }
+];
+
+console.log(productos);
